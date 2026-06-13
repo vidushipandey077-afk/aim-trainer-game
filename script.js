d95a748276c485752ff8d3a237db6b326b855feb
Vidushi Pandey <vidushipandey077@gmail.com>
2026-06-14 04:38:21 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const start=document.getElementById("startBtn")
 const score=document.getElementById("score")
 const timer=document.getElementById("time")
@@ -91,73 +90,3 @@ function changePos(){
 }
 start.addEventListener('click',startGame)
 resetBtn.addEventListener('click',startGame)
-=======
-const start=document.getElementById("startBtn")
-const score=document.getElementById("score")
-const timer=document.getElementById("time")
-const target=document.getElementById("target");
-const area=document.getElementById("gameArea")
-
-
-let moveInterval;
-let timeInterval;
-let game=false
-
-function startGame(){
-    game=true
-    target.style.left = "0px";
-    target.style.top = "0px";
-
-    score.textContent=0;
-    timer.textContent=30
-    target.style.display="block";
-
-    clearInterval(moveInterval);
-    clearInterval(timeInterval)
-
-    moveInterval=setInterval(changePos,1000);
-    timeInterval=setInterval(decreaseTimer,1000)
-}
-
-target.addEventListener('click',scoreInc)
-
-function scoreInc(){
-    if(!game){
-        return;
-    }
-    score.textContent=Number(score.textContent)+1;
-}
-
-function decreaseTimer(){
-    if(!game){
-        return
-    }
-    let curr=Number(timer.textContent)
-    if(curr>0){
-        timer.textContent=Number(timer.textContent)-1;
-    }
-    else{
-        stopGame()
-    }
-}
-
-function stopGame(){
-
-    game=false
-    clearInterval(moveInterval);
-    clearInterval(timeInterval)
-
-    target.style.display="none"
-    
-    alert("Game is Over!! You Score is: "+score.textContent)
-
-}
-
-function changePos(){
-    const x=Math.floor(Math.random()*650);
-    const y=Math.floor(Math.random()*400)
-    target.style.left=x+"px";
-    target.style.top=y+"px";
-}
-start.addEventListener('click',startGame)
->>>>>>> 5b3209f7b47fe86758318220bbfda91bb4c76730
